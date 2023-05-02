ea30fde65c0be9ba51bf04684039ecccc79d54c5
manik1241 <thmanik1241@gmail.com>
2023-05-02 15:02:55 +0600

    banner section

--- src/assets/components/Home/Home.jsx
+++ src/assets/components/Home/Home.jsx
@@ -1,9 +1,78 @@
+import  { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import './Home.css'
 
 
 const Home = () => {
+
+    const [index, setIndex] = useState(0);
+  
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+    };
+
+ 
+
     return (
-        <div>
-            <h2>this is home</h2>
+        <div className='my-5 carousel-data'>
+            <Carousel className='carousel ' activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+       <div className='d-flex row'>
+        <div className='col-sm-12 col-md-6'>
+        <img
+          className="d-block carousel-img "
+          src="../../../../public/bhuna-khichuri.png"
+          alt="First slide"
+        />
+        </div>
+        <div className=' col-sm-12 col-md-6 p-4 mt-4'>
+            <h4 className='text-success'>Our Special</h4>
+            <h2>Bhuna Khichuri</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ex ab id repudiandae sequi soluta quisquam,</p>
+            <button className='btn btn-outline-success' >Order Now</button>
+        </div>
+       </div>
+        
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className='d-flex  row'>
+         <div className='col-sm-12 col-md-6'>
+         <img
+          className="d-block carousel-img "
+          src="../../../../public/biriyani.jpg"
+          alt="Second slide"
+        />
+         </div>
+        <div className='col-sm-12 col-md-6 p-4 mt-4' >
+            <h4 className='text-success'>Our Special</h4>
+            <h2>Biriyani</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ex ab id repudiandae sequi soluta quisquam,</p>
+            <button className='btn btn-outline-success' >Order Now</button>
+        </div>
+        </div>
+
+        
+      </Carousel.Item>
+      <Carousel.Item>
+       <div className='d-flex  row'>
+         <div className='col-sm-12 col-md-6'>
+         <img
+          className="d-block carousel-img "
+          src="../../../../public/morog-polau.jpg"
+          alt="Third slide"
+        />
+         </div>
+        <div className='col-sm-12 col-md-6 p-4 mt-4'>
+        <h4 className='text-success'>Our Special</h4>
+            <h2>Morog Polau</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ex ab id repudiandae sequi soluta quisquam,</p>
+            <button className='btn btn-outline-success' >Order Now</button>
+        </div>
+       </div>
+
+  
+      </Carousel.Item>
+    </Carousel>
         </div>
     );
 };
